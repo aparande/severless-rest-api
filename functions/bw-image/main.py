a4d5e8f24dee5ed41c23ea7c185f85b22d2cfec7
Anmol Parande <parandea1.7@gmail.com>
2021-09-22 23:29:12 -0700

    Initial commit

---
+++ functions/bw-image/main.py
@@ -0,0 +1,58 @@
+"""
+Cloud function to convert an image to black and white.
+Triggered on image upload to Google Cloud Storage.
+"""
+
+import tempfile
+import os
+from typing import Any
+
+from PIL import Image
+from google.cloud import storage
+
+storage_client = storage.Client()
+
+def convert_image(from_file: str, to_file: str) -> None:
+  """
+  Convert an image to black and white using PIL
+
+  from_file: The image file that should be converted
+  to_file: The file to save the image to
+  """
+
+  with Image.open(from_file) as img:
+    img.convert('L').save(to_file)
+
+def bw_image(event: dict[str, Any], *_) -> None:
+  """
+  Cloud function entrypoint
+  """
+  # TODO: Extract the bucket name and the filename from the event
+  bucket = event['bucket']
+  filename = event['name']
+
+  # TODO: If the image is in the raw folder, don't process it
+  components = filename.split('/')
+  if components[0] != "raw":
+    print(f"{filename} is already processed!")
+    return
+
+  extension = '.' + components[-1].split('.')[-1]
+  _, download_file = tempfile.mkstemp(suffix=extension)
+  _, blurred_file = tempfile.mkstemp(suffix=extension)
+
+  # TODO: Download the image to download_file
+  blob = storage_client.bucket(bucket).blob(filename)
+  blob.download_to_filename(download_file)
+
+  # Process the Image
+  convert_image(download_file, blurred_file)
+
+  # TODO: Upload the image to the processed folder
+  new_filename = "/".join(["processed"] + components[1:])
+  print(f"Uploading to {new_filename}")
+  storage_client.bucket(bucket).blob(new_filename).upload_from_filename(blurred_file)
+
+  # Clean-Up
+  os.remove(download_file)
+  os.remove(blurred_file)
---
+++ functions/compile-gif/main.py
@@ -0,0 +1,89 @@
+"""
+Cloud function which compiles frames saved to Google Cloud into an APNG
+Triggered by HTTP request
+
+Headers:
+  Content-Type: application/json
+Query:
+  rate: int
+Body:
+  frames: list[str]
+"""
+
+import tempfile
+import os
+import hashlib
+
+from google.cloud import storage
+from apng import APNG
+
+storage_client = storage.Client()
+
+# TODO: Load environment variables
+bucket = os.environ['BUCKET']
+img_folder = os.environ['IMAGE_FOLDER']
+gif_folder = os.environ['GIF_FOLDER']
+
+def cleanup_tmp_files(files: list[str]) -> None:
+  """
+  Delete files from the provided list of files
+  """
+  for fname in files:
+    os.remove(fname)
+
+def save_frame(fname: str) -> str:
+  """
+  Save a frame from a bucket to the local disk
+  """
+
+  blob = storage_client.bucket(bucket).blob(img_folder + "/" + fname)
+  ext = fname.split('.')[-1]
+  _, tmp_file = tempfile.mkstemp(suffix='.' + ext)
+  blob.download_to_filename(tmp_file)
+  return tmp_file
+
+def process_frames(frames: list[str], frame_rate: int) -> str:
+  """
+  Compile a list of frame files into an APNG
+  Returns the filename of the apng
+  """
+  _, tmp_file = tempfile.mkstemp(suffix=".png")
+  APNG.from_files(frames, delay= int(1000 / frame_rate)).save(tmp_file)
+  return tmp_file
+
+def compile_gif(request) -> dict[str, str]:
+  """
+  Cloud function entrypoint
+  """
+
+  # TODO: Extract the frame rate from the request query
+  frame_rate = int(request.args["rate"])
+
+  # TODO: Extract the list of frames from the request body
+  request_json = request.get_json(silent=True)
+  frames = request_json["frames"]
+
+  # TODO: Use an MD5 hash of the filenames
+  name_str = '#'.join(frames)
+  md5_hash = hashlib.md5(name_str.encode()).hexdigest()
+  blob = storage_client.bucket(bucket).blob(gif_folder + "/" + md5_hash+".png")
+  if blob.exists():
+    return {
+      "Status": "Success",
+      "Message": "Already Converted GIF",
+      "URL": blob.public_url
+    }
+
+  frames = [save_frame(name) for name in frames]
+  apng_file = process_frames(frames, frame_rate)
+
+  blob.upload_from_filename(apng_file)
+  cleanup_tmp_files([apng_file] + frames)
+  blob.make_public()
+
+  # TODO: Return the HTTP Response body containing the public url
+  return {
+    "Status": "Success",
+    "Message": "Successfully converted frames to GIF",
+    "URL": blob.public_url
+  }
